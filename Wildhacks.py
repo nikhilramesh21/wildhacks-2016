6cc36e1abca708ba43209c928eec70c625134606
Nikhil Ramesh <nikhilramesh2019@u.northwestern.edu>
2016-11-19 16:24:34 -0600

    Python file to query the Northwestern ASG API and return a list of all Courses offered in a term

---
+++ Wildhacks.py
@@ -0,0 +1,40 @@
+
+
+import urllib
+import json 
+import pprint
+import requests
+
+################################################
+	#RETURNING A LIST OF ALL SUBJECTS IN A TERM
+################################################
+
+#to find all the subjects offered this quarter
+params = {
+  'key': '2EJqD5PCbVie7742',
+  'term': 4640,
+}
+response = requests.get('http://api.asg.northwestern.edu/subjects/', params=params)
+sublist = set([x["symbol"] for x in response.json()])
+sublist = list(sublist)
+
+
+
+####################################################################################
+	#RUNNING THE API TO GENERATE A LIST OF ALL COURSES OFFERED IN A TERM BY SUBJECT
+####################################################################################
+courses = {}
+for subject in sublist:
+	courses[subject] = []
+	params1 = {
+	  'key': '2EJqD5PCbVie7742',
+	  'term': 4640,
+	  'subject': subject
+	}
+	response1 = requests.get('http://api.asg.northwestern.edu/courses/', params=params1)
+	
+	catalognumbers = set([x["catalog_num"] for x in response1.json()])
+	courses[subject] = list(catalognumbers)
+
+
+
